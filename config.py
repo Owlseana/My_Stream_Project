63c7982fe7812ebff79791808ad6031d2dab3422
Owlseana <163309064+Owlseana@users.noreply.github.com>
2025-06-18 11:30:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # key与secret需要向b站申请开发者入驻，appId也是申请后才有
-idCode="EHIGG6WU4NWG6"  # 主播身份码
-appId=1743819179633  # 应用id
-key="jyANcIZicKvYTa1Fk6e9KGpE"  # access_key
-secret="klZnWjLuwVcYwxCKVx5JcFqFOCsybf"  # access_key_secret
+idCode=""  # 主播身份码
+appId=  # 应用id
+key=""  # access_key
+secret=""  # access_key_secret
 
 # 发送弹幕功能需要用到以下参数
-csrf_token = "93f8682aef74a6132048afd8f2dbc6d9"
-cookie = "buvid4=F9BABD5F-6EEC-1CFA-00D2-518F8227A14B04911-022030109-QmgoENZl2oOwlVyKFmCdRg%3D%3D; PVID=1; theme_style=light; buvid3=15B0A735-2111-12A6-C352-B45B28C76A2390403infoc; CURRENT_FNVAL=4048; buvid_fp_plain=undefined; share_source_origin=WEIXIN; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1720945506; Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1720945506; HMACCOUNT=126CE29F4FE1AE8B; bsource=search_bing; header_theme_version=CLOSE; enable_web_push=DISABLE; home_feed_column=5; b_nut=100; b_lsid=F2E8109A3_195EF8DB7FA; _uuid=9443D9AF-BCE2-C61B-9A67-BDC108143E108597187infoc; enable_feed_channel=ENABLE; browser_resolution=1492-771; SESSDATA=ab23e0a3%2C1759032893%2C269d6%2A42CjDple7_bT60AXgijgpWOxlP7BfC7bSobDRJSOs5URNHsbVXceSDYOB_0LQ8NkoTDB4SVkp6S09MTEYxV3RlNVcyd2tkNXV2dk9GWFZQNEw0TE5neWM3bzNPd3RsQzdSSGlBSm9tSGJtQXdwaEJhdXE5T1B3V1RsN1dkRXRMeDl0NFdWZDFVTFlnIIEC; bili_jct=93f8682aef74a6132048afd8f2dbc6d9; DedeUserID=1983066; DedeUserID__ckMd5=46b1243c7dd32d97; sid=g35jk8ey; fingerprint=de94bacb3f54a091390c5cf5b229aa04; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDM3NDAxMjcsImlhdCI6MTc0MzQ4MDg2NywicGx0IjotMX0.3jhZamwf0hBqlsDZJNcYXOsGYm0MNJ3wY5AroXwwEhA; bili_ticket_expires=1743740067; LIVE_BUVID=AUTO3517434809277240; buvid_fp=de94bacb3f54a091390c5cf5b229aa04"
-roomid = "6737549" # 房间id号
+csrf_token = ""
+cookie = ""
+roomid = "" # 房间id号
